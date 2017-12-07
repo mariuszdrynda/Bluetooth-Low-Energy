6f4f96102400ed1450228291aca9adf6111c6630
demono01 <tomek03f@gmail.com>
2017-12-07 03:14:48 +0100

    Permission do pamieci na zapis, potrzebne do stworzenia folderu na karcie pamieci. Tam beda nastepnie pliki potrzebne do zapisu archiwum wiadomosci.

--- BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/Activity_Chat.java
+++ BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/Activity_Chat.java
@@ -1,10 +1,12 @@
 package pz.bluetoothlowenergy;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Activity_Chat extends AppCompatActivity {
 
@@ -26,5 +28,13 @@ public class Activity_Chat extends AppCompatActivity {
                 startActivity(toy);
             }
         });
+    }
+    public void initZapisz(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Zapisuje";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
