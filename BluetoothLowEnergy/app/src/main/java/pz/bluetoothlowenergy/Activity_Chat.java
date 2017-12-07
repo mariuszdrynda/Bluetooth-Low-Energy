204537815bee9649f8feb43bcc39c249f971edc4
Unknown <mariuszdrynda@gmail.com>
2017-12-07 18:24:37 +0100

    Sporo zmian

--- BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/Activity_Chat.java
+++ BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/Activity_Chat.java
@@ -1,10 +1,18 @@
 package pz.bluetoothlowenergy;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.nio.charset.Charset;
 
 public class Activity_Chat extends AppCompatActivity {
 
@@ -15,6 +23,20 @@ public class Activity_Chat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__chat);
         initWsteczLobby();
+
+        incomingMessage = (TextView) findViewById(R.id.incomingMessage);
+        messages = new StringBuilder();
+        btnSend = (Button) findViewById(R.id.btnSend);
+        etSend = (EditText) findViewById(R.id.editText);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, new IntentFilter("incomingMessage"));
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                byte[] bytes = etSend.getText().toString().getBytes(Charset.defaultCharset());
+                Activity_Lobby.mBluetoothConnection.write(bytes);
+                etSend.setText("");
+            }
+        });
     }
 
     private void initWsteczLobby() {
@@ -27,4 +49,16 @@ public class Activity_Chat extends AppCompatActivity {
             }
         });
     }
+    StringBuilder messages;
+    TextView incomingMessage;
+    Button btnSend;
+    EditText etSend;
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("theMessage");
+            messages.append(text+"\n");
+            incomingMessage.setText(messages);
+        }
+    };
 }
--- BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/MainActivity.java
+++ BluetoothLowEnergy/app/src/main/java/pz/bluetoothlowenergy/MainActivity.java
@@ -5,37 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button login;
-
+    public EditText Nick;
+    public static String text;
     public void init(){
         login=(Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
-                Intent toy = new Intent(MainActivity.this,Activity_Lobby.class);
-
-                startActivity(toy);
+                if(!Nick.toString().equals("")){
+                    text = Nick.toString();
+                    Intent toy = new Intent(MainActivity.this,Activity_Lobby.class);
+                    startActivity(toy);
+                }
             }
         });
-
-
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Nick = (EditText) findViewById(R.id.editText1);
         init();
-
-
     }
-
-
-
 }
